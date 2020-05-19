acb673b48de71b3dd8c17a98a1bdcb6f4c125843
Fillip <greatdrummer1@gmail.com>
2020-05-19 15:35:02 +0300

    2 solution with semaphore fix

--- src/main/java/org/fidoshenyata/Pract2/Main.java
+++ src/main/java/org/fidoshenyata/Pract2/Main.java
@@ -1,14 +1,19 @@
 package org.fidoshenyata.Pract2;
 
+import java.util.concurrent.Semaphore;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         Data d = new Data();
+        Semaphore mutex = new Semaphore(1);
 
-        Worker w2 = new Worker(2, d);
-        Worker w1 = new Worker(1, d);
-        Worker w3 = new Worker(3, d);
+        Worker w2 = new Worker(2, d, mutex);
+        Worker w1 = new Worker(1, d, mutex);
+        Worker w3 = new Worker(3, d, mutex);
 
+        w1.join();
+        w2.join();
         w3.join();
         System.out.println("end of main...");
     }
--- src/main/java/org/fidoshenyata/Pract2/Worker.java
+++ src/main/java/org/fidoshenyata/Pract2/Worker.java
@@ -2,14 +2,18 @@ package org.fidoshenyata.Pract2;
 
 import lombok.SneakyThrows;
 
+import java.util.concurrent.Semaphore;
+
 public class Worker extends Thread {
 
     private int id;
     private final Data data;
+    private final Semaphore mutex;
 
-    public Worker(int id, Data d) {
+    public Worker(int id, Data d, Semaphore mutex) {
         this.id = id;
         data = d;
+        this.mutex = mutex;
         this.start();
     }
 
@@ -18,18 +22,16 @@ public class Worker extends Thread {
     public void run() {
         super.run();
         for (int i = 0; i < 5; i++) {
-            synchronized (data) {
-                while (data.getState() != id) data.wait();
-                if (id == 1) {
-                    data.Tic();
-                } else if (id == 2) {
-                    data.Tak();
-                } else {
-                    data.Toy();
-                }
-                data.notifyAll();
+            while (data.getState() != id) mutex.release();
+            mutex.acquire();
+            if (id == 1) {
+                data.Tic();
+            } else if (id == 2) {
+                data.Tak();
+            } else {
+                data.Toy();
             }
-
+            mutex.release();
         }
     }
 
